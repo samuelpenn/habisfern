21ba069088c5ee43a6153d3137ef74f6908c9e01
Samuel Penn <sam@glendale.org.uk>
2015-09-13 10:55:57 +0100

    Updated map drawing script to include more countries.

--- maps/cartexography/getall.sh
+++ maps/cartexography/getall.sh
@@ -3,11 +3,40 @@
 # Script to download images for each area in the world of Eorthe.
 # Requires a Cartexography server running locally.
 
+function getmap() {
+    area=$1
+    wget --output-document $area.png "http://localhost:8080/cartexography/image/area/eorthe?areaId=$area&s=0&border=2&areas=1&hex=1&style=hires"
+}
+
 mkdir -p weidany
 cd weidany
-for i in aidan northmarch northwood lanuch harholt langly estwed cenicsdyke halecester northwood strathby westland scarondy merrik merkia cadwold ornwold talmoth hain emrend midlaen angsly
+for i in aidan angsly cadwold cenicsdyke emrend estwed hain halecester harholt langly lanuch merkia merrik midlaen northmarch northwood ornwold scarondy strathby talmoth westland
 do
-    wget --output-document $i.png "http://localhost:8080/cartexography/image/area/eorthe?areaId=$i&s=0&border=2&areas=1&hex=1"
+echo    getmap $i
 done
 cd ..
 
+mkdir -p bryteland
+cd bryteland
+for i in bjornstathr erding harholt jarneyland nethvollr oforley sonodalr
+do
+    getmap $i
+done
+cd ..
+
+
+mkdir -p forest
+cd forest
+for i in cancha danuch niemstarg scarondy taneith tanwall
+do
+    getmap $i
+done
+cd ..
+
+mkdir -p north
+cd north
+for i in herzon niemstarg salov urazen
+do
+    getmap $i
+done
+cd ..
